6f710cabb3ac34d84f2eadc964690a8269486ee1
Nori Ishii <noriishii@Noris-MacBook-Pro.local>
2020-12-18 10:35:11 +1100

    mongoDB set up with Heroku

--- server.js
+++ server.js
@@ -17,8 +17,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+
 });
 
 
