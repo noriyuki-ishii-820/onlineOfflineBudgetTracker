8b10f9fd73c20ef54b99370a9d288730d66c8de7
Nori Ishii <noriishii@Noris-MacBook-Pro.local>
2020-12-18 13:00:07 +1100

    sw updated

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,6 +8,7 @@ const FILES_TO_CACHE = [
   "/index.js",
   "/styles.css",
   "/manifest.webmanifest",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
 ];
 
 // install
